8ffd3d2be4df97af58dcec6c0adeba742e5d2587
Ali <lisalmanov84@gmail.com>
2022-08-07 19:08:12 +0400

    task2

---
+++ homework2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace homework2
+{
+    internal class Program
+    {
+        private static int count;
+       
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine(count);
+        }
+        static string VowelsLetter(string[] letter)
+        {
+
+            string text = "Salam Aleykum";
+            Type vowelscount = null;
+            const vowels = null;
+
+            for (int i = 0; i < letter.Length; i++) 
+            {
+                if (text =[text[i] == 'a' || text[i] == 'e' || text[i] == 'i' || text[i] == 'o' || text[i] == 'u' || text[i] == 'ö' || text[i] == 'ü' || text[i] == 'ı' || text[i] == 'ə']) ;
+
+
+                else
+
+                    count++;
+            }
+
+
+        }
+    }
+}
---
+++ homework2/vowels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace homework2
+{
+    internal class vowels
+    {
+        public static implicit operator vowels(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
